320c777c7f71350bcfb30b8a173afadd1deed26f
Jonas2 Fischer <jonas2.fischer@st.oth-regensburg.de>
2022-03-07 20:15:50 +0100

    Add pyautogui

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import time
+import pyautogui
 
 
-# Press the green button in the gutter to run the script.
+def dive(duration):
+    pyautogui.keyDown('left')
+    print("dive\n")
+    time.sleep(duration)
+    pyautogui.keyUp('left')
+    print("don't dive\n")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    screen_width, screen_height = pyautogui.size()
+    pyautogui.moveTo(screen_width/2, screen_height/2, duration=1)
+    pyautogui.click()
+    for x in range(10):
+        dive(1)
+        time.sleep(1)
